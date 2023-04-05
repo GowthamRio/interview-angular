8e0e3d916dd46c2583f5d22d4470d9d85dd5a9ca
GowthamRio <129876110+GowthamRio@users.noreply.github.com>
2023-04-05 22:51:27 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-app',
@@ -11,5 +12,27 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
   }
+  person=[
+    {firstName:"Akhil",	lastName:"Kumar",	email:"akhil@gmail.com"	,phoneNumber:9959515110,	company:"BuzzBoard",birthday:"14-03-1991"},
+    {firstName:"Rahul",	lastName:"Dev",	email:"rahul@gmail.com"	,phoneNumber:8923193993,	company:"Infosys",birthday:"09-07-1990"},
+    {firstName:"Sampath",	lastName:"Kumar",	email:"sam@gmail.com"	,phoneNumber:9703037744,	company:"Cognizant",birthday:"27-06-1989"},
+  ];
+  
+  registerReactiveForm=new FormGroup({
+    firstName : new FormControl[""],
+    lastName: new FormControl[""],
+    email: new FormControl[""],
+    phoneNumber:new FormControl[""],
+    company:new FormControl[""],
+    gender: new FormControl[""],
+    birthday: new FormControl[""],
+    password: new FormControl[""],
+    confirmPassword: new FormControl[""],
 
+
+  });
+
+  onSubmit(data:any){
+    this.person.push(this.registerReactiveForm.value)
+  }
 }
